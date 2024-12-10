92e2761cb3f439a290c2025fe529ddcd809e6cf6
unknown <omar.er3434@gmail.com>
2024-12-11 01:17:08 +0200

    first commit

--- src/Layouts/SiteLayout.jsx
+++ src/Layouts/SiteLayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../Components/Header/Header'
 import { Outlet } from 'react-router-dom'
-import Footer from '../Components/Footer/Footer'
+// import Footer from '../Components/Footer/Footer'
 
 export default function SiteLayout() {
 
